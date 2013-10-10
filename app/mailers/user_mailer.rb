9e4dc93e69814bbf25f1620de631d88dd5bbf8ef
Sebastian Quack <sebastianquack@gmail.com>
2013-10-10 11:07:23 +0200

    update page title

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ActionMailer::Base
   
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
   
