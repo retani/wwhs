c818b2812542696f33b7f59eb75932ad3fd294fd
Sebastian Quack <sebastianquack@gmail.com>
2013-10-11 17:54:22 +0200

    precompile all assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Wwhs::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 
