d98433c1feb86cae5d98f7208a0a06252b0bdaa4
Sebastian Quack <sebastianquack@gmail.com>
2013-10-11 11:27:59 +0200

    clean up and add amazon images

--- app/models/media_link.rb
+++
@@ -1,5 +0,0 @@
-class MediaLink < ActiveRecord::Base
-  attr_accessible :challenge_id, :design_id, :ip, :url, :hidden
-  belongs_to :design
-  belongs_to :challenge
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,13 @@
 ActiveRecord::Schema.define(:version => 20131009121340) do
 
   create_table "images", :force => true do |t|
-    t.string   "ip"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "media_links", :force => true do |t|
-    t.string   "url"
-    t.string   "ip"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "title"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   create_table "pages", :force => true do |t|
