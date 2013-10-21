e6b1a3e4eaef1cc2ea7ff55d6352a3176b566895
Sebastian Quack <sebastianquack@gmail.com>
2013-10-21 19:17:49 +0200

    add next function to uchronist model

--- app/models/uchronist.rb
+++ app/models/uchronist.rb
@@ -4,4 +4,13 @@ class Uchronist < ActiveRecord::Base
   attr_accessible :description, :image_id, :media_embed, :name, :hidden, :uchronia_ids
 	belongs_to :image
 	has_and_belongs_to_many :uchronias
+
+	def next_uchronist
+		u = self.class.first(:conditions => ["name > ?", name], :order => "name asc")
+	  unless u
+	  	u = self.class.first(:order => "name asc")
+	  end
+	  return u
+	end
+
 end
