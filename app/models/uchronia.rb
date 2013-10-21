0e758c2a2ab7411c62463510d022f9273db5ab6d
Sebastian Quack <sebastianquack@gmail.com>
2013-10-21 15:15:55 +0200

    previous and next uchronias

--- app/models/uchronia.rb
+++ app/models/uchronia.rb
@@ -4,4 +4,21 @@ class Uchronia < ActiveRecord::Base
   attr_accessible :description, :image_id, :media_embed, :pod, :title, :hidden, :slug, :uchronist_ids
 	belongs_to :image
 	has_and_belongs_to_many :uchronists
+
+	def previous_uchronia
+	  u = self.class.last(:conditions => ["title < ?", title], :order => "title asc")
+	  unless u
+	  	u = self.class.last(:order => "title asc")
+	  end
+	  return u
+	end
+
+	def next_uchronia
+		u = self.class.first(:conditions => ["title > ?", title], :order => "title asc")
+	  unless u
+	  	u = self.class.first(:order => "title asc")
+	  end
+	  return u
+	end
+
 end
